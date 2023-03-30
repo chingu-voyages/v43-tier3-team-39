afe201dc94ff0d3a7c0e4e48e945efb69b787165
Andrew Lederman <andrewlederman@Andrews-MacBook-Pro-2.local>
2023-03-30 10:48:26 -0700

    feed first pass + NavMenu component

---
+++ TeamForward/client/src/components/NavMenu/NavMenu.jsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import { NavLink } from 'react-router-dom'
+
+const NavMenu = () => {
+
+    const [open,setOpen] = useState(false)
+
+    const dropDown = () => {
+        setOpen(!open)
+    }
+
+  return (
+    <div className="p-2 inline-block h-2/3">
+        <button onClick={dropDown}><div className="p-4 bg-blue-600 rounded-full shadow-lg inline-block">
+            <svg
+              className="h-10 w-10 text-white"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              {" "}
+              
+            </svg>
+          </div> </button> 
+        {
+            open ? (
+                <div className="absolute bg-opacity-100">
+                    <ul className="bg-opacity-100">
+                        <li className="block">
+                        <button className="text-lg font-semibold border p-1 rounded-lg shadow-lg block ">Profile</button>
+                        </li>
+                        <li className="block">
+                        <button className="text-lg font-semibold border p-1 rounded-lg shadow-lg block">Virtual Meets</button>
+                        </li>
+                        <li className="">
+                        <NavLink to="/updateprofile" className="text-lg font-semibold border p-1 rounded-lg shadow-lg block">Edit User Info</NavLink>
+                        </li>
+                        <li className="">
+                        <button className="text-lg font-semibold border p-1 rounded-lg shadow-lg block">Messages</button>
+                        </li>
+                        <li className="">
+                        <button className="text-lg font-semibold border p-1 rounded-lg shadow-lg block">Logout</button>
+                        </li>
+                    </ul>
+                </div>
+            ): null
+        }
+    </div>
+  )
+}
+
+export default NavMenu
