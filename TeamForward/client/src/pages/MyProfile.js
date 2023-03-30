ae6f5be5aee9da89b99e5ca00348144c4d8154bd
Hanson Tram <htram.dev@gmail.com>
2023-03-30 12:09:49 -0700

    add my profile

--- TeamForward/client/src/pages/MyProfile.js
+++ TeamForward/client/src/pages/MyProfile.js
@@ -4,23 +4,18 @@ import log from "../helpers/logging";
 import { userState } from "../GlobalState";
 import { useParams} from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useReactiveVar } from "@apollo/client";
 
 const UserProfile = () => {
-  const [userInfo, setUserInfo] = useState();
-  const user = userState();
-  const { id } = useParams();
 
-  useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_BE_URL}/teamForward/${id}`)
-      .then((res) => {
-        log(res.data)
-        setUserInfo(res.data);
-      })
-      .catch((err) => {
-        log(err);
-      });
-  });
+  const user = userState()
+  console.log('this is userState', user)
+  
+  // useEffect(() => {
+  //   userState()
+  //   console.log('useEffect hit')
+  // },); 
+
 
   return (
     <div className="p-16">
@@ -51,32 +46,36 @@ const UserProfile = () => {
               Connect
             </button>
             <button className="text-white py-2 px-4 uppercase rounded bg-gray-700 hover:bg-gray-800 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
-              Message
+              Edit Profile
             </button>
           </div>
         </div>
 
         <div className="mt-20 text-center border-b pb-12">
-          <h1 className="text-4xl font-medium text-gray-700">John Doe
-            {/* {user.firstName} */}
+          <h1 className="text-4xl font-medium text-gray-700">
+            {`${user.firstName} ${user.lastName}`}
           </h1>
-          <p className="font-light text-gray-600 mt-3">Bucharest, Romania</p>
+          <p className="font-light text-gray-600 mt-3">Location: {`${user.zipCode}`}</p>
 
-          <p className="mt-8 text-gray-500">
-            Solution Manager - Creative Tim Officer
+          <p className="mt-5 text-gray-500">
+            {user.profession}
           </p>
-          <p className="mt-2 text-gray-500">University of Computer Science</p>
+          {/* <p className="mt-2 text-gray-500">University of Computer Science</p> */}
         </div>
 
-        <div className="mt-12 flex flex-col justify-center">
+        <div className="mt-12 flex flex-col justify-center ">
           <p className="text-gray-600 text-center font-light lg:px-16">
-            An artist of considerable range, Ryan — the name taken by
-            Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and
-            records all of his own music, giving it a warm, intimate feel with a
-            solid groove structure. An artist of considerable range.
+            {user.bio}
           </p>
-          <h3>Interests</h3>
-          <h3>Activities</h3>
+        </div>
+        {/* INTERESTS AND ACTIVITIES */}
+        <div className="mt-12 flex flex-col justify-center  text-gray-500">
+          <h3>Interests:</h3>
+          
+        </div>
+        <div className="mt-12 flex flex-col justify-center  text-gray-500">
+          
+          <h3>Activities:</h3>
         </div>
       </div>
       <Link to="/feed" >Back to feed</Link>
