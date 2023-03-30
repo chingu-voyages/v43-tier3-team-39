1e81e1aff76897f2f75e6860cec790221fd6c5a4
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-03-30 11:26:20 -0700

    update

--- TeamForward/server/Models/User.js
+++ TeamForward/server/Models/User.js
@@ -50,11 +50,9 @@ const UserSchema = new mongoose.Schema({
     type: {
       type: String, // Don't do `{ location: { type: String } }`
       enum: ['Point'], // 'location.type' must be 'Point'
-      required: true
     },
     coordinates: {
       type: [Number],
-      required: true
     }
   },
   // how to convert miles to km
