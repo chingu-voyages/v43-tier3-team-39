a5c53dadb6722953b4add0b627df9b91f464fa61
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-04-04 20:55:50 -0700

    added button and css editing
    redid tailwind for signin/signup
    added button to navigate to home

--- TeamForward/client/src/pages/SignIn.js
+++ TeamForward/client/src/pages/SignIn.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import SignInWEmail from "../components/SignUpAndSignInPopUps/SignInWEmail";
-import { Link, Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import log from "../helpers/logging";
 
 const SignIn = () => {
   const [emailLoginForm, setEmailLoginForm] = useState(false);
+  const navigate = useNavigate();
 
   const google = () => {
     window.open(process.env.REACT_APP_WINDOWKEY, "_self");
@@ -16,14 +17,19 @@ const SignIn = () => {
       <header className="m-5 bg-white">Team Forward</header>
       <div className="h-screen bg-white relative flex flex-col space-y-10 justify-center items-center">
         <div className="bg-white shadow-none flex flex-col justify-center items-center rounded p-6 w-80">
+          <div className="place-self-start">
+            <button
+              type="button"
+              onClick={() => {
+                navigate("/");
+              }}
+              // value={value}
+              className="items-start my-0 btext-blue-600 inline-flex items-center hover:text-white border border-blue-600 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2.5 py-1.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-900"
+            > Home
+            </button>
+          </div>
           <h1 className="text-3xl font-bold leading-normal mb-3">Sign In</h1>
-          <button
-            className="bg-white p-1 my-3 w-full border border-black"
-            onClick={() => setEmailLoginForm(true)}
-          >
-            Sign in with Email
-          </button>
-          {emailLoginForm ? <SignInWEmail /> : null}
+          <SignInWEmail />
           <div className="mb-6 flex items-center justify-between"></div>
           {/* <button
             className="bg-white p-1 w-full border border-black"
@@ -33,10 +39,6 @@ const SignIn = () => {
           <a href="/signup" className="p-1 underline">
             Create New User
           </a>
-          <Link 
-            to="/" 
-            className="p-1 underline"
-          >Home</Link>
         </div>
       </div>
     </div>
--- TeamForward/client/src/pages/SignUp.js
+++ TeamForward/client/src/pages/SignUp.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import SignUpWEmail from "../components/SignUpAndSignInPopUps/SignUpWEmail";
 import { userState } from "../GlobalState";
-import { Link, Navigate } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import BasicButtonStyling from "../components/Button/BasicButtonStyling";
 
 const SignUp = () => {
   const [emailSignUpForm, setEmailSignUpForm] = useState(false);
-
+  const navigate = useNavigate();
   const user = userState();
 
   const google = async () => {
@@ -15,31 +16,26 @@ const SignUp = () => {
 
   return (
     <div className="bg-white flex flex-col h-screen">
-      <header className="m-5 bg-white">Team Forward</header>
-      <div className="h-screen bg-white relative flex flex-col space-y-10 justify-center items-center">
+      <header className="m-5 bg-white">Team Forward</header>      
+      <div className="h-screen bg-white relative flex flex-col space-y-4 justify-center items-center">
         <div className="bg-white shadow-none flex flex-col justify-center items-center rounded p-6 w-80">
-          <h1 className="text-3xl font-bold leading-normal mb-3">Sign Up</h1>
-          <button
-            className="bg-white p-1 my-3 w-full border border-black"
-            onClick={() => setEmailSignUpForm(true)}
-          >
-            Sign up with Email
-          </button>
-          {emailSignUpForm ? <SignUpWEmail /> : null}
-          {/* <button
-            className="bg-white p-1 w-full border border-black"
-            onClick={google}
-          >
-            Sign up with Google
-          </button> */}
-          <p>Already have an account?</p>
-          <a href="/signin" className="p-1 underline">
-            Sign in
-          </a>
-          <Link 
-            to="/" 
-            className="p-1 underline"
-          >Home</Link>
+          <div className="place-self-start">
+            <button
+              type="button"
+              onClick={() => {
+                navigate("/");
+              }}
+              // value={value}
+              className="items-start my-0 btext-blue-600 inline-flex items-center hover:text-white border border-blue-600 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2.5 py-1.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-900"
+            > Home
+            </button>
+          </div>
+          <h1 className="text-3xl font-bold leading-normal mt-2 mb-3">Sign Up</h1>
+          <SignUpWEmail />
+            <p>Already have an account?</p>
+            <a href="/signin" className="p-1 underline">
+              Sign in
+            </a>
         </div>
       </div>
     </div>
