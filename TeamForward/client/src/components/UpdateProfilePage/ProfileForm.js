91503ca7a9819723504c68b40e1e31cc7914079d
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2023-04-15 11:02:27 -0700

    home page get started button alignment

--- TeamForward/client/src/components/UpdateProfilePage/ProfileForm.js
+++ TeamForward/client/src/components/UpdateProfilePage/ProfileForm.js
@@ -127,7 +127,7 @@ export default function ProfileForm({
                 htmlFor="radius"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                Radius (in miles):    {formInfo.radius}
+                Radius:    {formInfo.radius} miles
               </label>
               <input
                 type="range"
@@ -202,11 +202,11 @@ export default function ProfileForm({
           <div className="flex items-center space-x-4">
             <button
               type="submit"
-              className="text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              className="text-green-700 border border-green-600 hover:bg-green-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-6 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
             >
               Submit
             </button>
-            <button className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
+            <button className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-6 py-2 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
               <Link to="/feed">Cancel</Link>
             </button>
           </div>
